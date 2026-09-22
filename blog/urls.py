2d684e623c7dd00db8bdcd42549288165940c6b7
Marcus Leeper <admin@hangryherbivore.com>
2026-09-22 02:10:49 +0000

    fix: blog categories

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from .views import BlogListAPI, BlogDetailAPI, BlogCategoryListAPI
 
 urlpatterns = [
     path('api/blog/', BlogListAPI.as_view()),
-    path('api/blog/<slug:slug>/', BlogDetailAPI.as_view()),
     path('api/blog/categories/', BlogCategoryListAPI.as_view()),
+    path('api/blog/<slug:slug>/', BlogDetailAPI.as_view()),
 ]
